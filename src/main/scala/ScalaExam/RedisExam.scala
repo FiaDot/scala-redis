3a0e9313a3c5a54f3dc56bfd2faaebc5aa50e055
fiadot <fiadot@gmail.com>
2014-06-03 11:16:34 +0900

    close #1 : ping, get, set 테스트

--- src/main/scala/ScalaExam/RedisExam.scala
+++ src/main/scala/ScalaExam/RedisExam.scala
@@ -11,14 +11,45 @@ object RedisExam {
 		implicit val akkaSystem = akka.actor.ActorSystem()
 
 		val redis = RedisClient("192.168.229.134", 6379)		
-
+		
 		val futurePong = redis.ping()
 		println("Ping sent!")
 		futurePong.map(pong => {
 			println(s"Redis replied with a $pong")
 		})
 		Await.result(futurePong, 5 seconds)
-
+		
+		
+		val req = redis.set("scalaredis", "100")	
+		
+		req.map( request => { 
+			println(s"req = $request")
+		})
+		
+		
+		val res = redis.get("scalaredis")
+			
+		var result = res.map(_.get)
+		var result2 = res.map(_.get).map(_.utf8String).map( response => println(s"res = $response"))
+		
+		println(s"$result")
+		println(s"$result2")
+		// .map( result => println(s"$result") )
+		//var result = res.map(_.get).map(_.utf8String)
+		
+		
+		//println(s"$result")
+		
+	
+				
+/*		
+		val futurePong = redis.ping()
+		println("Ping sent!")
+		futurePong.map(pong => {
+			println(s"Redis replied with a $pong")
+		})
+		Await.result(futurePong, 5 seconds)
+*/
 		akkaSystem.shutdown()
 		
 	}
